bbc0b6c6585c0e33ca7b96d672fe1091ccb5c9da
singaporezoo <singaporezoo060@gmail.com>
2022-06-07 09:36:04 +0000

    integrated previous with actual homeview (alien)

--- main/homeview.py
+++ main/homeview.py
@@ -36,79 +36,9 @@ def home():
         contestInfos = [i for i in contestInfos if (i["public"] or userinfo["username"] in i["users"])]
 
 
-    dateArr = []
-    memo = {}
-    for i in range(8):
-        date = datetime.now() - timedelta(days=i)
-        query = date.strftime('%Y-%m-%d')
-        # Looking for the LAST submission of the day
-        low = 0 
-        high = 200000
-        while high > low:
-            mid = int((low+high + 1)/2)
-            if mid in memo.keys():
-                submission = memo[mid]
-            else:
-                submission = awstools.getSubmission(mid)
-                memo[mid] = submission
-            if submission != None and query >= submission["submissionTime"]:
-                low=mid
-            else:
-                high=mid - 1
-        dateArr.append(low)
-    
-    dateMap = {}
-    for i in range(7):
-        date = datetime.now() - timedelta(days=i)
-        query = date.strftime('%Y-%m-%d')
-        dateMap[query] = dateArr[i] - dateArr[i+1]
-
+    subsPerDay = awstools.getSubsPerDay()
+    print(subsPerDay)
     credits_info = awstools.credits_page()
-    
-    #this is slow ><
-    """
-    lastWeek = datetime.now() - timedelta(days=8)
-    weekDate = lastWeek.strftime('%Y-%m-%d')
-    # Looking for the LAST submission of the day
-    low = 0 
-    high = 200000
-    while high > low:
-        mid = int((low+high + 1)/2)
-        if mid in memo.keys():
-            submission = memo[mid]
-        else:
-            submission = awstools.getSubmission(mid)
-            memo[mid] = submission
-        if submission != None and weekDate >= submission["submissionTime"]:
-            low=mid
-        else:
-            high=mid - 1
-
-    latest = int(awstools.getNumberOfSubmissions())
-    submissionList = []
-    for i in range(low + 1, latest + 1, 100):
-        response = awstools.batchGetSubmissions(i, min(i+100, latest+1)-1)
-        submissions = response['Responses']['codebreaker-submissions']
-        submissionList += submissions
-
-    dateMap = {}
-    problemMap = {}
-    for i in submissionList:
-        subTime = i['submissionTime']
-        date = subTime.strip().split(' ')[0]
-        if date not in dateMap:
-            dateMap[date] = 0
-        dateMap[date] += 1
-
-        problem = i['problemName']
-        if problem not in problemMap:
-            problemMap[problem] = 0
-        problemMap[problem] += 1
-
-    highest = sorted(problemMap, key=problemMap.get, reverse=True)[:5]
-    problemMap = dict([(i, problemMap[i]) for i in highest])
-
-    """
 
     return render_template('home.html',
                            userinfo=userinfo,
