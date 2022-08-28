627e188a63ee2438152f7da773bef148195322a7
singaporezoo <singaporezoo060@gmail.com>
2022-08-28 10:04:24 +0000

    linked homepage with data

--- main/homeview.py
+++ main/homeview.py
@@ -10,41 +10,28 @@ def home():
         return redirect(f'/contest/{contestmode.contestId()}')
 
     userinfo = awstools.getCurrentUserInfo()
-    languages_inverse = language.get_languages_inverse()
-    if userinfo != None:
-        userSubmissionList = awstools.getSubmissionsList(1, None, userinfo['username'])
-        userSubmissionList = userSubmissionList[:8]
-        for i in userSubmissionList:
-            i['language'] = languages_inverse[i['language']]
-    else:
-        userSubmissionList = None
-    globalSubmissionList = sorted(awstools.getSubmissionsList(1, None, None),key=lambda x:x["subId"], reverse=True)
-    globalSubmissionList = globalSubmissionList[:8]
-    for i in globalSubmissionList:
-        i['language'] = languages_inverse[i['language']]
-
     if userinfo != None:
         username = userinfo["username"]
     else:
         username = ""
-    
-    contestInfos = [i for i in awstools.getAllContests() if i["endTime"] != "Unlimited"]
+   
+    statistics = awstools.homepageInfo()
+    contestInfo = statistics['contests']
     if userinfo == None:
-        contestInfos = [i for i in contestInfos if i["public"]]
+        contestInfo = [i for i in contestInfo if i["public"]]
     elif "admin" not in userinfo["role"]:
-        contestInfos = [i for i in contestInfos if (i["public"] or userinfo["username"] in i["users"])]
+        contestInfo = [i for i in contestInfo if (i["public"] or userinfo["username"] in i["users"])]
 
-
-    subsPerDay = awstools.getSubsPerDay()
-    credits_info = awstools.homepageInfo()
+    dates = []
+    for i in range(7,0,-1):
+        day = datetime.now() - timedelta(days=i)
+        dates.append(day.strftime('%d/%m'))
 
     return render_template('home.html',
                            userinfo=userinfo,
-                           globalSubmissionList=globalSubmissionList,
-                           userSubmissionList=userSubmissionList,
-                           contestInfos=contestInfos,
-                           statistics=awstools.homepageInfo(),
+                           contestInfo = contestInfo,
+                           statistics=statistics,
                            socket=contestmode.socket(),
-                           subsPerDay=subsPerDay)
+                           dates=dates)
 
 
