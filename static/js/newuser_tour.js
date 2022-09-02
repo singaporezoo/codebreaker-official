b2912f8affb2641b9e8cdd9a60155f0bcb18ca9d
singaporezoo <singaporezoo060@gmail.com>
2022-09-02 05:33:34 +0000

    transferring

--- static/js/newuser_tour.js
+++ static/js/newuser_tour.js
@@ -5,22 +5,24 @@ var tour = new Tour({
     path: "",
     element: "#recently-uploaded-text",
     title: "Title of my step",
-    content: "Content of my step"
+    content: "Content of my step",
+    placement: "top"
   },
   {
     path: "",
     element: "#new-user-box",
     title: "Title of my next step",
-    content: "Content of my next step"
+    content: "Content of my next step",
+    placement: "left"
   },
   {
     path: "/problems",
     element: "#myTable",
     title: "testing the different page tour",
-    content: "Content of my next step"
+    content: "Content of my next step",
+    placement: "bottom"
   }],
-  backdrop: true,
-  storage: false
+  backdrop: true
 });
 tour.init();
 $("#start-tour-btn").click(function() {
