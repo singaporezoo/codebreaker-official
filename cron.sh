6e8c3f569559f9021cc0fbfcc45cf4e7976f5905
singaporezoo <singaporezoo060@gmail.com>
2024-07-03 08:48:45 +0000

    Pushed cronjob

---
+++ cron.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+PORT=5000
+SERVICE="codebreaker"  # Replace with the actual service name (e.g., "flask_app")
+
+if ! ss -tulpn | grep -q ":$PORT "; then
+    echo "$(date) - Port $PORT not in use, restarting $SERVICE" >> /var/log/check_port_5000.log
+    sudo systemctl restart $SERVICE
+else
+    echo "$(date) - Port $PORT in use" >> /var/log/check_port_5000.log
+fi
--- deploy.sh
+++
@@ -1,3 +0,0 @@
-sudo systemctl stop apache2
-sudo systemctl restart nginx
-sudo python3 /home/ubuntu/codebreaker-official/app.py deploy
