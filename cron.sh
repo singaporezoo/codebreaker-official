6bbdf0659db080c8523c0bea301bf67bf4b9b4b8
dvdg6566 <czhdaniel@gmail.com>
2024-10-22 09:48:37 +0100

    Updated cronsh command

--- cron.sh
+++ cron.sh
@@ -5,6 +5,8 @@ SERVICE="codebreaker"  # Replace with the actual service name (e.g., "flask_app"
 
 if ! ss -tulpn | grep -q ":$PORT "; then
     echo "$(date) - Port $PORT not in use, restarting $SERVICE" >> /var/log/check_port_5000.log
+    sudo systemctl stop apache2
+    sudo systemctl restart nginx
     sudo systemctl restart $SERVICE
 else
     echo "$(date) - Port $PORT in use" >> /var/log/check_port_5000.log
